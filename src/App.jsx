3eabac1c0623c1cf717134a622edf94ea20097e5
shuaib-cyclist <shuaib.cyclist@gmail.com>
2023-11-05 18:05:36 +0600

    Toaster added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Toaster } from "react-hot-toast";
 import { Outlet } from "react-router-dom";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
       <div className="font-inter">
         <Outlet></Outlet>
       </div>
+      <Toaster position="top-center" reverseOrder={false} />
     </>
   );
 }
