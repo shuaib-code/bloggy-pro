b4b06de1542f584b4804bf049f6ad761a8bb9f6b
shuaib-cyclist <shuaib.cyclist@gmail.com>
2023-11-08 00:47:29 +0600

    Exploring framer motion completed

--- src/components/banner/HomeBanner.jsx
+++ src/components/banner/HomeBanner.jsx
@@ -36,7 +36,22 @@ const HomeBanner = () => {
         </div>
       </div>
       <div>
-        <img src="/banner.png" alt="" />
+        <motion.img
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.5,
+            ease: [0, 0.71, 0.2, 1.01],
+            scale: {
+              type: "spring",
+              damping: 5,
+              stiffness: 150,
+              restDelta: 0.002,
+            },
+          }}
+          src="/banner.png"
+          alt=""
+        />
       </div>
     </div>
   );
